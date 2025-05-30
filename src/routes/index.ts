5e2371876ed2f4b2698aa4cd84379d216a566c48
Alex Maia <alex.ribeiro.maiaa@gmail.com>
2025-05-30 17:51:42 -0300

    feat: add login router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import clientRoutes from './clientRoutes';
 import userRoutes from './userRoutes';
+import loginController from '@/controllers/loginController';
 
 const router = express.Router();
 
 router.use('/clients', clientRoutes);
 router.use('/users', userRoutes);
+router.use('/auth/login', loginController.login)
 
 export default router
