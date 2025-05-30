816bfa505ee437f45edb455e2c1425527d4e24c2
Alex Maia <alex.ribeiro.maiaa@gmail.com>
2025-05-30 17:51:30 -0300

    feat: encrypting password

--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -4,6 +4,7 @@ import { UserEntity } from "@/entities/userEntity";
 import AppDataSource from "@/database/connection";
 import { CreateUserDTO } from "@/dto/create.userDto";
 import { UpdateUserDTO } from "@/dto/update.userDto";
+import { genSalt, hash, compare } from "bcrypt-ts";
 
 export default class UserRepository {
 
@@ -17,28 +18,55 @@ export default class UserRepository {
       return await this.repository.find()
     }
 
-    async getOneUser(id: string): Promise<UserEntity | null> {
+    async getOneUserById(id: string): Promise<UserEntity | null> {
       return await this.repository.findOneBy({ id })
     }
 
+    async getOneUserByEmail(email: string): Promise<UserEntity | null> {
+      return await this.repository.findOneBy({ email })
+    }
     async createUser(user:CreateUserDTO):Promise<UserEntity | null>{
+
+      const salt = await genSalt(10)
+      const result = await hash(user.password, salt)
+
       const newUser = new UserEntity
       newUser.name = user.name
       newUser.email = user.email
       newUser.cpf = user.cpf
-      newUser.password = user.password
+      newUser.password = result
+
       return await this.repository.save(newUser)
     }
 
-    async updateUser(user:UpdateUserDTO):Promise<UserEntity | null> {
-      const updateUser = await this.getOneUser(user.id)
+    async updateUser(user:UpdateUserDTO):Promise<UserEntity | object> {
+
+
+      const updateUser = await this.getOneUserById(user.id)
+
       if(!updateUser) {
-        return null
+        return {
+          status: 404,
+          errorMessage: "User ID not found"
+        }
       }
+
+      const verifyPassword = await compare(user.password, updateUser.password)
+
+      if(!verifyPassword) {
+        return {
+          status: 404,
+          errorMessage: "Incorrectly Password, please try again"
+        }
+      }
+
+      const salt = await genSalt(10)
+      const result = await hash(user.newPassword, salt)
+
       updateUser.name = user.name
       updateUser.email = user.email
       updateUser.cpf = user.cpf
-      updateUser.password = user.password
+      updateUser.password = result
 
       return await this.repository.save(updateUser)
     }
