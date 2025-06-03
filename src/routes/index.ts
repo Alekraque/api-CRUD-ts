692885628d30aa7fdb1bbfd1057a70b075dcfbed
Alex Maia <alex.ribeiro.maiaa@gmail.com>
2025-06-03 16:08:45 -0300

    feat: add error tratative in main routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,23 @@
-import express from 'express'
-import clientRoutes from './clientRoutes'
-import userRoutes from './userRoutes'
+import express, { Request, Response, NextFunction } from "express";
+import clientRoutes from "./clientRoutes";
+import userRoutes from "./userRoutes";
+import loginRoutes from "./loginRoutes";
+import { AuthError } from "@/utils/authError";
 
-import loginRoutes from './loginRoutes'
+const router = express.Router();
 
-const router = express.Router()
+router.use("/clients", clientRoutes);
+router.use("/users", userRoutes);
+router.use("/auth/login", loginRoutes);
 
-router.use('/clients', clientRoutes)
-router.use('/users', userRoutes)
-router.use('/auth/login', loginRoutes)
 
-export default router
+router.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof AuthError) {
+    return res.status(err.statusCode).json({ error: err.message })
+  }
+
+  console.error("Erro não tratado:", err);
+  return res.status(500).json({ error: "Erro interno no servidor" })
+})
+
+export default router;
