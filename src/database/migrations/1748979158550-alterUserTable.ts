50f9389cd25a14aad43e1e63fa83f5a7bb09c7ba
Alex Maia <alex.ribeiro.maiaa@gmail.com>
2025-06-03 16:50:23 -0300

    feat: run migration to add role column

--- src/database/migrations/1748979158550-alterUserTable.ts
+++ src/database/migrations/1748979158550-alterUserTable.ts
@@ -1,9 +1,14 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
 
 export class AlterUserTable1748979158550 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-      
+      await queryRunner.addColumn('users', new TableColumn({
+        name: "role",
+        type: "varchar",
+        length: "255",
+        default: "user"
+      }))
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
