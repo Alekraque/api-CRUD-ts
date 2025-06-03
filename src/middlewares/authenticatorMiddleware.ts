088fada60f1ee024662eeabb5361bbdd2179bd18
Alex Maia <alex.ribeiro.maiaa@gmail.com>
2025-06-03 14:28:49 -0300

    feat: create middleware: jwt authenticator

---
+++ src/middlewares/authenticatorMiddleware.ts
@@ -0,0 +1,45 @@
+import jwt from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
+
+interface tokenPayload {
+  id: string,
+  email: string
+}
+
+
+export const authenticateJWT = (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.sendStatus(401).json({
+      errorMessage: "No information about Token"
+    })
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  jwt.verify(token, process.env.JWT_SECRET as string, (err, decoded) => { //callback depois que o JWT for verificado
+    if (err || typeof decoded !== 'object' || !decoded) {
+      return res.sendStatus(403);
+    }
+
+  const payload = decoded as tokenPayload;
+
+  req.user = {
+    id: payload.id,
+    email: payload.email
+  }
+
+
+    if (typeof decoded === 'object' && 'id' in decoded) {
+      req.user = {
+        id: (decoded as any).id,
+      };
+      next();
+    } else {
+      return res.sendStatus(403).json({
+        errorMessage: "token format is invalid"
+      });
+    }
+  })
+}
