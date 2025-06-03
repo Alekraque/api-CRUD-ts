939f18a831265c7aedf38c57542cc3aa4fa73624
Alex Maia <alex.ribeiro.maiaa@gmail.com>
2025-06-03 14:35:04 -0300

    refactor: fix the main route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,13 @@
-import express from 'express';
-import clientRoutes from './clientRoutes';
-import userRoutes from './userRoutes';
-import loginController from '@/controllers/loginController';
+import express from 'express'
+import clientRoutes from './clientRoutes'
+import userRoutes from './userRoutes'
 
-const router = express.Router();
+import loginRoutes from './loginRoutes'
 
-router.use('/clients', clientRoutes);
-router.use('/users', userRoutes);
-router.use('/auth/login', loginController.login)
+const router = express.Router()
+
+router.use('/clients', clientRoutes)
+router.use('/users', userRoutes)
+router.use('/auth/login', loginRoutes)
 
 export default router
