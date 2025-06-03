2b2be97addc59ea68f7cec6853d9a98b098c7c6c
Alex Maia <alex.ribeiro.maiaa@gmail.com>
2025-06-03 16:10:12 -0300

    refactor: change the default code

--- src/utils/authError.ts
+++ src/utils/authError.ts
@@ -1,7 +1,7 @@
 export class AuthError extends Error {
   statusCode: number;
 
-  constructor(message: string, statusCode = 401) {
+  constructor(message: string, statusCode = 400) {
     super(message);
     this.name = 'AuthError';
     this.statusCode = statusCode;
