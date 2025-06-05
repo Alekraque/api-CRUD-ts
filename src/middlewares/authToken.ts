a8ed6fdc09b99d039b44a022625134b8bb61f50b
Alex Maia <alex.ribeiro.maiaa@gmail.com>
2025-06-05 16:24:03 -0300

    remove: delete console.logs

--- src/middlewares/authToken.ts
+++ src/middlewares/authToken.ts
@@ -5,8 +5,6 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const authToken = (req: Request, res: Response, next: NextFunction) => {
-  console.log('JWT_SECRET_TOKEN:', process.env.JWT_SECRET_TOKEN)
-  console.log('JWT_TOKEN_ADMIN:', process.env.JWT_TOKEN_ADMIN)
   const authHeader = req.headers.authorization
 
   if (!authHeader) {
@@ -20,14 +18,12 @@ export const authToken = (req: Request, res: Response, next: NextFunction) => {
     let verifyToken = jwt.verify(token, process.env.JWT_SECRET_TOKEN) as tokenPayload //Tenta verificar o token usando o secret do user normal
 
     req.user = { id: verifyToken.id, role: verifyToken.role } //Salva os dados do user verificado no objeto req.user, para poder usar depois nas rotas.
-    console.log("Usuário autenticado como user:", req.user)
     return next() // proximo middleware ou controller ou rota tanto faz, depende do que vier primeiro
   } catch (error1) { // se falhar com a verificacao do user, eh pq o token nao eh do tipo user
     try {
       // Se falhar aquele trycatch, eu tento dnv com o admin
       let verifyToken = jwt.verify(token, process.env.JWT_TOKEN_ADMIN) as tokenPayload //Tenta verificar o token usando o secret do user ADMIN
       req.user = { id: verifyToken.id, role: verifyToken.role } //salva os dados do admin verificado no req.user
-      console.log("Usuário autenticado como admin:", req.user)
       return next() // se for valido, ele vai pro proximo, segue o jogo
     } catch (error2) { // se nenhuma das validacoes der certo, ou seja,
                        // se nenhum dos tokens forem validos, eles nao sao nem admin e nem user
